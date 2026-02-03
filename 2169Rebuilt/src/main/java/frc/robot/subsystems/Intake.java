45432d038be2dd341f44c28b81235168e570b7d8
Khanh <khanhngongnguyen2008@gmail.com>
2026-02-02 19:03:22 -0600

    more getters and setters + some intake constants

--- 2169Rebuilt/src/main/java/frc/robot/subsystems/Intake.java
+++ 2169Rebuilt/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,13 +1,15 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import frc.robot.Constants;
+import frc.robot.Constants.IntakeConstants;
 
 public class Intake {
     private TalonFX pivotMotor;
     private TalonFX spinMotor;
 
-    private final double pivotMaxHeight;
-    private final double pivotMinHeight;
+    private final double pivotMaxHeight = IntakeConstants.pivotMaxHeight;
+    private final double pivotMinHeight = IntakeConstants.pivotMinHeight;
 
     public Intake(){
         pivotMotor = new TalonFX(Constants.Ports.pivotMotor);
@@ -16,7 +18,21 @@ public class Intake {
 
     // Setters
 
-    public void setVoltagePivot(double voltage){
-        pivotMotor.setVoltage(voltage);
+    public void setVoltagePivot(double volts){
+        pivotMotor.setVoltage(volts);
+    }
+
+    public void setVoltageSpin(double volts){
+        spinMotor.setVoltage(volts);
+    }
+
+    // Getters
+
+    public double getVelocitySpin(){
+        return spinMotor.getVelocity().getValueAsDouble();
+    }
+
+    public double getVelocityPivot(){
+        return pivotMotor.getVelocity().getValueAsDouble();
     }
 }
