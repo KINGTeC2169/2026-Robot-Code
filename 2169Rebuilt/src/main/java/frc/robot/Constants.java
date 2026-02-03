86f59dd17b15c5d82aa8900fc9e9e630a8acc7a1
Khanh <khanhngongnguyen2008@gmail.com>
2026-02-02 19:03:22 -0600

    more getters and setters + some intake constants

--- 2169Rebuilt/src/main/java/frc/robot/Constants.java
+++ 2169Rebuilt/src/main/java/frc/robot/Constants.java
@@ -1,5 +1,13 @@
 package frc.robot;
 
 public final class Constants {
+    public static final class Ports{
+        public static final int pivotMotor = 0;         // TODO: REPLACE WITH REAL MOTOR ID
+        public static final int spinMotor = 0;          // TODO: REPLACE WITH REAL MOTOR ID
+    }
 
+    public static final class IntakeConstants{
+        public static final double pivotMaxHeight = 0;  // TODO: REPLACE WITH REAL NUM
+        public static final double pivotMinHeight = 0;  // TODO: REPLACE WITH REAL NUM
+    }
 }
