322ab15801f601287bdba389d2d7591b3995f472
Khanh <khanhngongnguyen2008@gmail.com>
2026-02-02 19:45:23 -0600

    pid for pivot

--- 2169Rebuilt/src/main/java/frc/robot/subsystems/Intake.java
+++ 2169Rebuilt/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,12 +1,19 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.IntakeConstants;
 
-public class Intake {
+public class Intake extends SubsystemBase{
     private TalonFX pivotMotor;
     private TalonFX spinMotor;
+
+    private PIDController pivotPID;
 
     private final double pivotMaxHeight = IntakeConstants.pivotMaxHeight;
     private final double pivotMinHeight = IntakeConstants.pivotMinHeight;
@@ -14,6 +21,8 @@ public class Intake {
     public Intake(){
         pivotMotor = new TalonFX(Constants.Ports.pivotMotor);
         spinMotor = new TalonFX(Constants.Ports.spinMotor);
+
+        pivotPID = new PIDController(IntakeConstants.kP, IntakeConstants.kI, IntakeConstants.kD);
     }
 
     // Setters
@@ -26,6 +35,10 @@ public class Intake {
         spinMotor.setVoltage(volts);
     }
 
+    public void setPivotPosition(double position){
+        position = MathUtil.clamp(position, pivotMinHeight, pivotMaxHeight);
+    }
+
     // Getters
 
     public double getVelocitySpin(){
@@ -34,5 +47,11 @@ public class Intake {
 
     public double getVelocityPivot(){
         return pivotMotor.getVelocity().getValueAsDouble();
+    }
+
+    @Override
+    public void periodic(){
+        SmartDashboard.putNumber("Pivot Velocity", getVelocityPivot());
+        SmartDashboard.putNumber("Spin Velocity", getVelocitySpin());
     }
 }
