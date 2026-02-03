494acf8b3b108b7a57804d1a0faf7b44bb8ac8be
Khanh <khanhngongnguyen2008@gmail.com>
2026-02-02 18:22:25 -0600

    Intake test commit

--- 2169Rebuilt/src/main/java/frc/robot/subsystems/Intake.java
+++ 2169Rebuilt/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 public class Intake {
-    
+    private TalonFX pivotMotor;
+    private TalonFX spinMotor;
+
+    private final double pivotMaxHeight;
+    private final double pivotMinHeight;
+
+    public Intake(){
+        pivotMotor = new TalonFX(Constants.Ports.pivotMotor);
+        spinMotor = new TalonFX(Constants.Ports.spinMotor);
+    }
+
+    // Setters
+
+    public void setVoltagePivot(double voltage){
+        pivotMotor.setVoltage(voltage);
+    }
 }
