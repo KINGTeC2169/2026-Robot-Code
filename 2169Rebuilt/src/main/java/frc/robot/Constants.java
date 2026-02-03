c9301a029203a43bc7fbbe340f9a0b82469d4660
Khanh <khanhngongnguyen2008@gmail.com>
2026-02-02 19:40:29 -0600

    pid for pivot

--- 2169Rebuilt/src/main/java/frc/robot/Constants.java
+++ 2169Rebuilt/src/main/java/frc/robot/Constants.java
@@ -9,5 +9,9 @@ public final class Constants {
     public static final class IntakeConstants{
         public static final double pivotMaxHeight = 0;  // TODO: REPLACE WITH REAL NUM
         public static final double pivotMinHeight = 0;  // TODO: REPLACE WITH REAL NUM
+
+        public static final double kP = 0;  // TODO: REPLACE WITH REAL KP
+        public static final double kI = 0;  // TODO: REPLACE WITH REAL KI
+        public static final double kD = 0;  // TODO: REPLACE WITH REAL KD
     }
 }
