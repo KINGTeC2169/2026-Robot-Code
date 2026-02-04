50945e4164c11d9ed728aa706a588c0527f7ec91
Khanh <khanhngongnguyen2008@gmail.com>
2026-02-03 18:32:51 -0600

    added LL getters, constructor, and constants

--- 2169Rebuilt/src/main/java/frc/robot/Constants.java
+++ 2169Rebuilt/src/main/java/frc/robot/Constants.java
@@ -2,4 +2,9 @@ package frc.robot;
 
 public final class Constants {
 
+    public final class Vision{
+        private static double angle = 0;    // TODO: REPLACE WITH REAL ANGLE
+        private static double height = 0;   // TODO: REPLACE WITH REAL HEIGHT 
+    }
+
 }
--- 2169Rebuilt/src/main/java/frc/robot/subsystems/Limelight.java
+++ 2169Rebuilt/src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,5 +1,27 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Limelight {
+    private static NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
     
+    public Limelight(){
+        SmartDashboard.putNumber("tx", getTx());
+        SmartDashboard.putNumber("ty", getTy());
+    }
+
+    // Getters
+    public double getTx(){
+        return table.getEntry("tx").getDouble(0);
+    }
+
+    public double getTy(){
+        return table.getEntry("ty").getDouble(0);
+    }
+
+    public double getID(){
+        return table.getEntry("tid").getDouble(0);
+    }
 }
