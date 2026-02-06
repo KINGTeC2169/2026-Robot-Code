ccd9a344e95f0724c5e1bf0683f72ea9126aca96
Khanh <khanhngongnguyen2008@gmail.com>
2026-02-06 17:32:03 -0600

    added getters for hood LL and LimelightHelpers file

--- 2169Rebuilt/src/main/java/frc/robot/Constants.java
+++ 2169Rebuilt/src/main/java/frc/robot/Constants.java
@@ -3,8 +3,13 @@ package frc.robot;
 public final class Constants {
 
     public final class Vision{
-        private static double angle = 0;    // TODO: REPLACE WITH REAL ANGLE
-        private static double height = 0;   // TODO: REPLACE WITH REAL HEIGHT 
+        public static double[] blueHubTags = {18, 27, 19, 20, 26, 25, 21, 24};
+        public static double[] redHubTags = {8, 5, 9, 10, 4, 3, 11, 2};
+
+        public static double hoodAngle = 0;                 // TODO: REPLACE WITH REAL ANGLE
+        public static double hoodHeight = 0;                // TODO: REPLACE WITH REAL HEIGHT
+        public static double hubTagHeight = 21.75;      // inches
+        public static String hoodLimelightName = "name";    // TODO: REPLACE WITH HOOD LL NAME
     }
 
 }
--- 2169Rebuilt/src/main/java/frc/robot/subsystems/Limelight.java
+++ 2169Rebuilt/src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,52 +1,61 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants;
+import frc.robot.Constants.Vision;
+import Util.LimelightHelpers;
 
 public class Limelight {
-    private static NetworkTable table1 = NetworkTableInstance.getDefault().getTable("limelight 1");
-    private static NetworkTable table2 = NetworkTableInstance.getDefault().getTable("limelight 2");
-    
-    public Limelight(){
-        SmartDashboard.putNumber("tx LL 1", getTxLL1());
-        SmartDashboard.putNumber("ty LL 1", getTyLL1());
-        SmartDashboard.putNumber("id LL 1", getIDLL1());
 
-        SmartDashboard.putNumber("tx LL 2", getTxLL2());
-        SmartDashboard.putNumber("ty LL 2", getTyLL2());
-        SmartDashboard.putNumber("id LL 2", getIDLL2());
+    private static String hoodName = Constants.Vision.hoodLimelightName;
+  
+    public Limelight(){
+        SmartDashboard.putNumber("tx", getTx());
+        SmartDashboard.putNumber("id", getID());
     }
 
     // Getters
     // get tx from limelight 1
     // where tx is the horizontal offset from center of april tag to LL crosshair
-    public double getTxLL1(){
-        return table1.getEntry("tx").getDouble(0);
+    public double getTx(){
+        return LimelightHelpers.getTX(hoodName);
     }
-    // get tx from limelight 1
-    // where ty is the vertical offset from center of april tag to LL crosshair
-    public double getTyLL1(){
-        return table1.getEntry("ty").getDouble(0);
-    }
-    // get tid from limelight 1
-    // where tid is the id of the tag
-    public double getIDLL1(){
-        return table1.getEntry("tid").getDouble(0);
-    }
-    // get tx from limelight 2
+    // get ty from limelight 1
     // where tx is the horizontal offset from center of april tag to LL crosshair
-    public double getTxLL2(){
-        return table2.getEntry("tx").getDouble(0);
+    public double getTy(){
+        return LimelightHelpers.getTY(hoodName);
     }
-    // get tx from limelight 2
-    // where ty is the vertical offset from center of april tag to LL crosshair
-    public double getTyLL2(){
-        return table2.getEntry("ty").getDouble(0);
+    // get id from limelight 1
+    // where id is the id of the tag
+    public double getID(){
+        return LimelightHelpers.getFiducialID(hoodName);
     }
-    // get tid from limelight 2
-    // where tid is the id of the tag
-    public double getIDLL2(){
-        return table2.getEntry("tid").getDouble(0);
+    // checks if the limelight is looking
+    // at one of the hub april tags
+    public boolean isValidID(){
+        double id = getID();
+        Alliance color = DriverStation.getAlliance().get();
+        
+        if(color == Alliance.Red){
+            for(double tag : Constants.Vision.redHubTags){
+                if(id == tag) return true;
+            }
+        }
+        
+        if (color == Alliance.Blue) {
+            for(double tag : Constants.Vision.blueHubTags){
+                if(id == tag) return true;
+            }
+        
+        }
+
+        return false;
+    }
+    // Distance from tag in inches
+    // Might need some constant value added or multiplied
+    public double getDistance(){
+        return ((Vision.hubTagHeight - Vision.hoodHeight) / Math.tan(Vision.hoodAngle + getTy())) +22;
     }
 }
