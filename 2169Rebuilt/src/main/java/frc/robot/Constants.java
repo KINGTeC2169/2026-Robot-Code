6f49b6ebcaff5a20ae9f4dc0f8b8b1134370a36b
Driver Station <kingtecprogramming@gmail.com>
2026-03-02 18:03:54 -0600

    I fixed things

--- 2169Rebuilt/src/main/java/frc/robot/Constants.java
+++ 2169Rebuilt/src/main/java/frc/robot/Constants.java
@@ -55,4 +55,14 @@ public final class Constants {
         public static PIDConstants autoTranslationPID = new PIDConstants(0, 0, 0);
         public static PIDConstants autoRotationPID = new PIDConstants(0, 0, 0);
     }
+
+    public final class Vision{
+        public static final String blueHubTags = "18/27/19/20/26/25/21/24";
+        public static final String redHubTags = "8/5/9/10/4/3/11/2";
+
+        public static final double hoodAngle = 0;                 // TODO: REPLACE WITH REAL ANGLE
+        public static final double hoodHeight = 0;                // TODO: REPLACE WITH REAL HEIGHT
+        public static final double hubTagHeight = 44.25;          // inches
+        public static final String hoodLimelightName = "name";    // TODO: REPLACE WITH HOOD LL NAME
+    }
 }
