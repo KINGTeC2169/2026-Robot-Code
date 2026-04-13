fa53a57cdcb56d38fe483ecacb8cf958f4e52c3a
Ethan Mathew <ethmatt09@gmail.com>
2026-04-12 21:10:34 -0500

    Pitch is negative

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -45,8 +45,8 @@ public final class Constants {
         public static final Translation3d BACK_RIGHT_TRANSLATION = new Translation3d(-0.26035, -0.31115, 0.24765);
         public static final Translation3d FRONT_TRANSLATION = new Translation3d(0.3048, 0, .1651);
 
-        public static final Rotation3d FRONT_LEFT_ROTATION = new Rotation3d(Units.degreesToRadians(180),Units.degreesToRadians(53), Units.degreesToRadians(45)); 
-        public static final Rotation3d BACK_RIGHT_ROTATION = new Rotation3d(Units.degreesToRadians(180),Units.degreesToRadians(53), Units.degreesToRadians(225));
+        public static final Rotation3d FRONT_LEFT_ROTATION = new Rotation3d(Units.degreesToRadians(180),Units.degreesToRadians(-53), Units.degreesToRadians(45)); 
+        public static final Rotation3d BACK_RIGHT_ROTATION = new Rotation3d(Units.degreesToRadians(180),Units.degreesToRadians(-53), Units.degreesToRadians(225));
 
         public static final double FRONT_CAMERA_HEIGHT_METERS = .24765; //TODO: Replace with correct value
         public static final double FRONT_CAMERA_PITCH_RADIANS = 0; 
