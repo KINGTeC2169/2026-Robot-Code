6215a95be8ad35aa36c7a86af4f5b41aaf3c8cd4
Ethan Mathew <ethmatt09@gmail.com>
2026-05-09 16:02:19 -0500

    Added vision

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotBase;
 
 /**
@@ -50,5 +54,22 @@ public final class Constants {
 
     // 1.25 max boost = 125% faster
     public static final double outtakeMaxBoost = 0.5;
+  }
+
+  public final class Vision{
+
+      //PhotonVision stuff 
+      public static final Translation3d FRONT_LEFT_TRANSLATION = new Translation3d(0.26035, 0.31115, 0.24765);
+      public static final Translation3d BACK_RIGHT_TRANSLATION = new Translation3d(-0.26035, -0.31115, 0.24765);
+      public static final Translation3d FRONT_TRANSLATION = new Translation3d(0.3048, 0, 0.1651);
+
+      public static final Rotation3d FRONT_LEFT_ROTATION = new Rotation3d(Units.degreesToRadians(0),Units.degreesToRadians(-53), Units.degreesToRadians(45)); 
+      public static final Rotation3d BACK_RIGHT_ROTATION = new Rotation3d(Units.degreesToRadians(0),Units.degreesToRadians(-53), Units.degreesToRadians(225));
+
+      public static final Transform3d FRONT_LEFT_CAMERA_TO_ROBOT = new Transform3d(FRONT_LEFT_TRANSLATION, FRONT_LEFT_ROTATION);
+      public static final Transform3d BACK_RIGHT_CAMERA_TO_ROBOT = new Transform3d(BACK_RIGHT_TRANSLATION, BACK_RIGHT_ROTATION);
+
+      public static final double FRONT_CAMERA_HEIGHT_METERS = .24765; //TODO: Replace with correct value
+      public static final double FRONT_CAMERA_PITCH_RADIANS = 0; 
   }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -32,6 +32,11 @@ import frc.robot.subsystems.drive.GyroIOPigeon2;
 import frc.robot.subsystems.drive.ModuleIO;
 import frc.robot.subsystems.drive.ModuleIOSim;
 import frc.robot.subsystems.drive.ModuleIOTalonFX;
+import frc.robot.subsystems.vision.Vision;
+import frc.robot.subsystems.vision.VisionIO;
+import frc.robot.subsystems.vision.VisionIOPhotonVision;
+import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
+
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -43,6 +48,7 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 public class RobotContainer {
   // Subsystems
   private final Drive drive;
+  private final Vision vision;
   private final Intake intake;
   private final LED led;
 
@@ -76,6 +82,9 @@ public class RobotContainer {
                 new ModuleIOTalonFX(TunerConstants.BackLeft),
                 new ModuleIOTalonFX(TunerConstants.BackRight));
             
+        vision = new Vision(drive::addVisionMeasurement,
+                            new VisionIOPhotonVision("Front_Left_Camera", Constants.Vision.FRONT_LEFT_CAMERA_TO_ROBOT),
+                            new VisionIOPhotonVision("Back_Right_Camera", Constants.Vision.BACK_RIGHT_CAMERA_TO_ROBOT));
         intake = new Intake();
         led = new LED();
 
@@ -107,6 +116,9 @@ public class RobotContainer {
                 new ModuleIOSim(TunerConstants.FrontRight),
                 new ModuleIOSim(TunerConstants.BackLeft),
                 new ModuleIOSim(TunerConstants.BackRight));
+        vision = new Vision(drive::addVisionMeasurement,
+                            new VisionIOPhotonVisionSim("Front_Left_Camera", Constants.Vision.FRONT_LEFT_CAMERA_TO_ROBOT, drive::getPose),
+                            new VisionIOPhotonVisionSim("Back_Right_Camera", Constants.Vision.BACK_RIGHT_CAMERA_TO_ROBOT, drive::getPose));
         intake = new Intake();
         led = new LED();
         break;
@@ -120,6 +132,7 @@ public class RobotContainer {
                 new ModuleIO() {},
                 new ModuleIO() {},
                 new ModuleIO() {});
+        vision = new Vision(drive::addVisionMeasurement, new VisionIO() {}, new VisionIO() {});
         intake = new Intake();
         led = new LED();
         break;
